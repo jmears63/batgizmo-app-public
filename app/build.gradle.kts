2776f9d1e371cd4c684e25f775eef6aa02005f19
John Mears <john@themears.co.uk>
2026-09-12 15:16:35 +0100

    Release 2.1.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "uk.org.gimell.batgizmoapp"
         minSdk = 29
         targetSdk = 37
-        versionCode = 31
-        versionName = "2.1.0"
+        versionCode = 32
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
